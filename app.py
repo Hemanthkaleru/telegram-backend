22c79b12d6d8bee327b69edcd9c8954c4cdb5687
Hemanth Kaleru <kaleruhemanth19@gmail.com>
2026-05-06 01:33:51 +0530

    backend

--- app.py
+++ app.py
@@ -1,31 +1,20 @@
-from flask import Flask, request, jsonify
-import requests
-import os
-
-app = Flask(__name__)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-@app.route("/health")
-def health():
-    return {"status": "ok"}
-
 @app.route("/send", methods=["POST"])
 def send():
     data = request.json
 
     text = f"👤 {data['user']}\n💬 {data['message']}\n🤖 {data['reply']}"
 
-    try:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-        requests.post(url, json={
-            "chat_id": CHAT_ID,
-            "text": text
-        }, timeout=5)
+    res = requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
 
-        return jsonify({"status": "sent"})
+    print("Telegram status:", res.status_code)
+    print("Telegram response:", res.text)
 
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    return {
+        "status": "sent",
+        "telegram_response": res.text
+    }
